3289ee5f673ba3c1c973940fbb2c35354273602d
ahacad <ahacad@126.com>
2021-07-18 22:55:06 +0800

    chore(gulpfile): update gulp to latest and fix gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,36 +1,31 @@
 'use strict';
 
-var fs           = require('fs');
-var path         = require('path');
-var gulp         = require('gulp');
-var brfs         = require('brfs');
-var source       = require('vinyl-source-stream');
-var connect      = require('gulp-connect');
-var sass         = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');
-var reactify     = require('reactify');
-var browserify   = require('browserify');
-var watchify     = require('watchify');
-var uglify       = require('gulp-uglify');
-var replace      = require('gulp-replace');
+const fs = require('fs');
+const path = require('path');
+const gulp = require('gulp');
+const brfs = require('brfs');
+const source = require('vinyl-source-stream');
+const connect = require('gulp-connect');
+const sass = require('gulp-sass')(require('node-sass'));
+const autoprefixer = require('gulp-autoprefixer');
+const reactify = require('reactify');
+const browserify = require('browserify');
+const watchify = require('watchify');
+const uglify = require('gulp-uglify');
+const replace = require('gulp-replace');
 
-gulp.task('default', ['set-version', 'lib', 'style'], function () {
-  gulp.watch('./stylesheets/**/*.scss', ['style']);
+const parallel = gulp.parallel;
+const series = gulp.series;
 
-  return connect.server({
-    root: ['dist'],
-    port: 8000,
-    livereload: true
-  });
-}); 
-
-gulp.task('lib', function () {
-  var bundler = watchify(browserify({
-    cache: {},
-    packageCache: {},
-    fullPaths: true,
-    extensions: '.jsx'
-  }));
+function lib() {
+  const bundler = watchify(
+    browserify({
+      cache: {},
+      packageCache: {},
+      fullPaths: true,
+      extensions: '.jsx',
+    }),
+  );
 
   bundler.add('./lib/init.js');
   bundler.transform(reactify);
@@ -38,9 +33,10 @@ gulp.task('lib', function () {
 
   bundler.on('update', rebundle);
 
-  function rebundle () {
+  function rebundle() {
     console.log('rebundling');
-    return bundler.bundle()
+    return bundler
+      .bundle()
       .on('error', function (err) {
         console.log(err.message);
       })
@@ -50,46 +46,55 @@ gulp.task('lib', function () {
   }
 
   return rebundle();
-});
-
-gulp.task('style', function () {
-  return gulp.src('./stylesheets/main.scss')
-    .pipe(sass({errLogToConsole: true, outputStyle: 'compressed'}))
+}
+function minify() {
+  return gulp.src('./dist/js/*').pipe(uglify()).pipe(gulp.dest('./dist/js'));
+}
+function css() {
+  return gulp
+    .src('./stylesheets/main.scss')
+    .pipe(sass({ errLogToConsole: true, outputStyle: 'compressed' }))
     .pipe(autoprefixer())
     .pipe(gulp.dest('./dist/css'))
     .pipe(connect.reload());
-});
+}
 
-gulp.task('set-version', function () {
-  return gulp.src('./dist/index.html')
-  .pipe(replace(/\?v=([\w\.]+)/g, '?v=' + require('./package.json').version))
-  .pipe(gulp.dest('./dist/'))
-  .pipe(connect.reload());
-});
+function setVersion() {
+  return gulp
+    .src('./dist/index.html')
+    .pipe(replace(/\?v=([\w\.]+)/g, '?v=' + require('./package.json').version))
+    .pipe(gulp.dest('./dist/'))
+    .pipe(connect.reload());
+}
 
-gulp.task('schemes', function () {
-  var source = 'dist/schemes';
-  var index = 'index.json';
-  var output = [];
+function schemes() {
+  const source = 'dist/schemes';
+  const index = 'index.json';
+  let output = [];
 
   fs.readdirSync(source).forEach(function (folder) {
     if (folder !== index) {
+      const files = fs.readdirSync(path.join(source, folder));
 
-      var files = fs.readdirSync(path.join(source, folder));
-
-      output = output.concat(files.map(function (file) {
-        return path.join(folder, path.basename(file, '.json'));
-      }));
-
+      output = output.concat(
+        files.map(function (file) {
+          return path.join(folder, path.basename(file, '.json'));
+        }),
+      );
     }
   });
 
   fs.writeFileSync(path.join(source, index), JSON.stringify(output));
-});
+}
 
-gulp.task('minify', function () {
-  return gulp.src('./dist/js/*')
-    .pipe(uglify())
-    .pipe(gulp.dest('./dist/js'));
-});
-
+function defaultFunc() {
+  // gulp.watch('./stylesheets/**/*.scss', ['style']);
+  // return connect.server({ root: ['dist'], port: 8000, livereload: true });
+}
+exports.default = function () {
+  gulp.watch(
+    ['stylesheets/*', 'lib/*'],
+    { ignoreInitial: false },
+    parallel(series(parallel(series(lib, minify), css, setVersion)), schemes),
+  );
+};
